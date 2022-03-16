5d61e08769a166e9f34a6cceeef524ae76765a74
bany <bany.y0599@gmail.com>
2022-03-16 10:46:29 +0700

    curl-to-json: change index input

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const parse = require('./pare-json');
 
-var out = parse(`curl --location --request GET 'localhost:8002/v1/api/search?q=react' \
---header 'ss: asd ' \
---header 'Content-Type: application/x-www-form-urlencoded' \
---data-urlencode 'sd=dsb' \
---data-urlencode 'sds=sdsss'`);
+var out = parse(`curl -X --request GET 'http://localhost:3001/search' \
+--header 'xapi-key: 21432139847290402402190' \
+--header 'Content-Type: text/html' \
+--data-raw '{
+    "name": "string",
+    "name2": "string"
+}'`);
 
 // const out = parse(`curl --location --request GET 'http://google.com?search=react&asdfa=asfdaf' -H 'Accept-Encoding: gzip, deflate, sdch' -H 'Accept-Language: en-US,en;q=0.8,da;q=0.6' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Connection: keep-alive' --compressed`)
 
