ef74bb076cbe84d038a91b964dc1166b2ab57019
9bany <bany.y0599@gmail.com>
2022-05-24 15:31:14 +0700

    add example

---
+++ example/index.js
@@ -0,0 +1,25 @@
+const parse = require('@bany/curl-to-json');
+
+var out = parse(`curl -X GET 'http://localhost:3001/search?search=react&asdfa=asfdaf' \
+--header 'xapi-key: 21432139847290402402190' \
+--header 'Content-Type: text/html' \
+--data-raw '{
+    "name": "string",
+    "name2": "string"
+}'`);
+
+console.log(out)
+
+/**
+ * OUTPUT:
+ {
+  method: 'GET',
+  header: {
+    'xapi-key': '21432139847290402402190',
+    'Content-Type': 'text/html'
+  },
+  url: 'http://localhost:3001/search?search=react&asdfa=asfdaf',
+  origin: 'http://localhost:3001',
+  params: { search: 'react', asdfa: 'asfdaf' },
+  body: { name: 'string', name2: 'string' }
+} */
